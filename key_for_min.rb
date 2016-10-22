50f706747b7bce368009c6f396f7535554663287
Ahmed Mohammed <iahmed215@gmail.com>
2016-10-22 19:50:35 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+#  new_val=[]
+#name_hash.each_value do |val|
+#  new_val << val
+#end
+#small=new_val[0]
+#new_val.each do |index|
+#  if index<small
+#    small=index
+#  end
+#end
+#small
+all_number=[]
+ name_hash.collect do |name,number|
+ all_number << number
 end
+min=all_number.sort
+name_hash.key(min.first)
+end
+name_hash={:blake => 100, :ashley => 50, :adam => 17}
+key_for_min_value(name_hash)
