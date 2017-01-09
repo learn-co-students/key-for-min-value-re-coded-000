b9dbc3726092de6e6a2c63d8d053a6df34a67c30
Abdalkhalek ahmed <eng.abdalkalk@gmail.com>
2017-01-09 06:42:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,3 +11,15 @@ end
 end
 min_keys
 end
+
+
+#def key_for_min_value(name_hash)
+#  if !name_hash.empty?
+  #  name_hash.min.empty?
+    #name_hash.min_by{|key,value| value} [0]
+  #end
+#end
+#key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
+
+#min_value=name_hash.values.sort.first
+#name_hash.key(min_values)
